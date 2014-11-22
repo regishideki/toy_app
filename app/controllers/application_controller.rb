cf9ceb51b9aaa366ffe154993eb0fa34b9dacb82
Regis Hideki Hattori <regis_hattori@yahoo.com.br>
2014-11-22 17:42:01 +0000

    finish toy app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
-  def hello
-    render text: "Welcome to Toy APP"
-  end
+  #def hello
+  #  render text: "Welcome to Toy APP"
+  #end
   
 end
